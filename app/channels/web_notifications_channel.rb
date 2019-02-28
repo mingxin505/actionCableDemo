a4291c782ff358244ef72af410176a2ca6f1fc7b
wangzhanduo <wangzhanduo@offcn.com>
2019-02-28 20:46:42 +0800

    actionCable demo

---
+++ app/channels/web_notifications_channel.rb
@@ -0,0 +1,10 @@
+class WebNotificationsChannel < ApplicationCable::Channel
+  def subscribed
+		stream_from "web_notifications_channel"
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
